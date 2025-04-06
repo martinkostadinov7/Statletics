eedeefa8f6fa46e5ea3c8f4b4c5e0052b5e9d2c4
Martin Kostadinov <m.kostadinov07@gmail.com>
2025-04-07 00:22:26 +0300

    Add core entities and context for DataLayer
    - Introduced `Category`, `Discipline`, and `RunningRecord` classes in the BusinessLayer with properties and navigation relationships.
    - Added empty repository classes for the new entities in the DataLayer.
    - Created `StatleticsContext` class inheriting from `DbContext`, with DbSet properties for the new entities.
    - Established references to Entity Framework Core and MySQL Entity Framework Core in the DataLayer.
    - Created a solution file (DataLayer.sln) linking BusinessLayer and ServiceLayer.
    - Added empty service classes for `Category`, `Discipline`, and `RunningRecord` in the ServiceLayer.

---
+++ BusinessLayer/RunningRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer;
+
+public partial class RunningRecord
+{
+    public int Id { get; set; }
+
+    public int IdDiscipline { get; set; }
+
+    public DateTime DateTime { get; set; }
+
+    public TimeSpan Result { get; set; }
+
+    public bool? HasSpikes { get; set; }
+
+    public string? ActivityType { get; set; }
+
+    public bool? IsOutdoor { get; set; }
+}
---
+++ DataLayer/StatleticsContext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using BusinessLayer;
+namespace DataLayer;
+
+public partial class StatleticsContext : DbContext
+{
+    public StatleticsContext()
+    {
+    }
+
+    public StatleticsContext(DbContextOptions<StatleticsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Discipline> Disciplines { get; set; }
+
+    public virtual DbSet<RunningRecord> RunningRecords { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("Server= 127.0.0.1;Database=statletics;User Id=root;Password=root");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("categories");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("ID");
+            entity.Property(e => e.Name).HasMaxLength(60);
+        });
+
+        modelBuilder.Entity<Discipline>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("disciplines");
+
+            entity.HasIndex(e => e.IdCategory, "FK_Cat_disc");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("ID");
+            entity.Property(e => e.IdCategory)
+                .HasColumnType("int(11)")
+                .HasColumnName("ID_Category");
+            entity.Property(e => e.Name).HasMaxLength(60);
+
+            entity.HasOne(d => d.IdCategoryNavigation).WithMany(p => p.Disciplines)
+                .HasForeignKey(d => d.IdCategory)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_Cat_disc");
+        });
+
+        modelBuilder.Entity<RunningRecord>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("running_records");
+
+            entity.HasIndex(e => e.IdDiscipline, "ID_Discipline");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("ID");
+            entity.Property(e => e.ActivityType)
+                .HasMaxLength(60)
+                .HasDefaultValueSql("'NULL'");
+            entity.Property(e => e.DateTime).HasColumnType("datetime");
+            entity.Property(e => e.HasSpikes).HasDefaultValueSql("'NULL'");
+            entity.Property(e => e.IdDiscipline)
+                .HasColumnType("int(11)")
+                .HasColumnName("ID_Discipline");
+            entity.Property(e => e.IsOutdoor).HasDefaultValueSql("'NULL'");
+            entity.Property(e => e.Result).HasColumnType("time");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
